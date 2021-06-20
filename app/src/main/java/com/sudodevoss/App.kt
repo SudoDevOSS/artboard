baa96b8a5812fd4bf2eca700795a21c8d08a8078
SudoDevOSS <sliding_xp@protonmail.com>
2021-06-20 18:41:36 +0300

    Enable dark theme Remove toolbar

--- app/src/main/java/com/sudodevoss/App.kt
+++ app/src/main/java/com/sudodevoss/App.kt
@@ -1,6 +1,8 @@
 package com.sudodevoss
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.sudodevoss.artboard.ioc.DIConfigurator
 import com.sudodevoss.core.data.common.SharedNetworking
 import org.kodein.di.DI
@@ -16,6 +18,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         SharedNetworking.configureClient("https://www.mocky.io/v2/", "")
-//        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
     }
 }
