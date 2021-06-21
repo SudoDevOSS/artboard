a295e4b24669d73e76c7db2be5f4fc50ac1730ca
SudoDevOSS <sliding_xp@protonmail.com>
2021-06-21 05:48:31 +0300

    Add ExperimentalPagingApi annotation

--- app/src/main/java/com/sudodevoss/App.kt
+++ app/src/main/java/com/sudodevoss/App.kt
@@ -3,12 +3,13 @@ package com.sudodevoss
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.paging.ExperimentalPagingApi
 import com.sudodevoss.artboard.ioc.DIConfigurator
 import com.sudodevoss.core.data.common.SharedNetworking
 import org.kodein.di.DI
 
 class App : Application() {
-
+    @ExperimentalPagingApi
     companion object {
         val diContainer = DI {
             importAll(DIConfigurator.diModules)
